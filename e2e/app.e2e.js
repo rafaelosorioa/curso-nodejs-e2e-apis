078cd8ec02f8cca7eaa9ddfc7d49d8612ae457c5
Rafael Osorio <rafael.osorio@globant.com>
2023-09-04 12:26:39 -0500

    First e2e test

---
+++ e2e/app.e2e.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.status(200).json({ name: 'rafa' });
+});
+
+app.listen(9000);
+const api = request(app);
+
+describe('test for app', () => {
+  test('GET /hello', async () => {
+    const response = await api.get('/hello');
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.name).toEqual('rafa');
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+});
