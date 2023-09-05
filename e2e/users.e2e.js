2b8a1c2bb7227ec0e3d19dd5a87b49fb924ff180
Rafael Osorio <rafael.osorio@globant.com>
2023-09-05 11:26:14 -0500

    Test consulting the DB

--- e2e/users.e2e.js
+++ e2e/users.e2e.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 
 const createApp = require('../src/app');
+// Import the models from the DB
+const { models } = require('./../src/db/sequelize');
 
 let app = null;
 let server = null;
@@ -22,7 +24,22 @@ afterEach(() => {
 });
 
 describe('test for users', () => {
+  describe('GET /users/{id}', () => {
+    it('should return a user', async () => {
+      // Bring the user from the DB
+      // Direct validation to the DB
+      const user = await models.User.findByPk('1');
+
+      const { statusCode, body } = await api.get(`/api/v1/users/${user.id}`);
+
+      expect(statusCode).toEqual(200);
+      expect(body.id).toEqual(user.id);
+      expect(body.email).toEqual(user.email);
+    });
+  });
+
   describe('GET /users', () => {});
+
   describe('POST /users', () => {
     test('should return a 400 bad request', async () => {
       // Arrange
@@ -53,6 +70,7 @@ describe('test for users', () => {
       expect(statusCode).toEqual(400);
       expect(body.message).toMatch(/email/);
     });
+    //TODO:valid response and save test
   });
   describe('PUT /users', () => {});
 });
