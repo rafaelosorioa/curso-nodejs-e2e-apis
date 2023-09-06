d842e66095dd95522d9b18c9422b8eba7e0a7949
Rafael Osorio <rafael.osorio@globant.com>
2023-09-05 20:47:57 -0500

    Add test setting the headers

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-
+const { config } = require('./../src/config/config');
 const createApp = require('../src/app');
 
 let app = null;
@@ -28,5 +28,28 @@ describe('test for app', () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('rafa');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  describe('GET /nueva-ruta', () => {
+    it('should return 401 no apiKey', async () => {
+      const { statusCode } = await api.get('/nueva-ruta');
+
+      expect(statusCode).toEqual(401);
+    });
+
+    it('should return 401 with invalid apiKey', async () => {
+      const { statusCode } = await api
+        .get('/nueva-ruta')
+        .set({ api: 'anykey' });
+
+      expect(statusCode).toEqual(401);
+    });
+    it('should return 200', async () => {
+      const { statusCode } = await api
+        .get('/nueva-ruta')
+        .set({ api: config.apiKey });
+
+      expect(statusCode).toEqual(200);
+    });
   });
 });
