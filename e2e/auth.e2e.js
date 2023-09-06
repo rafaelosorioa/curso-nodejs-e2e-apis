2d09340e3fec2066866c2f0a4602415e3b754b65
Rafael Osorio <rafael.osorio@globant.com>
2023-09-05 20:23:12 -0500

    Add test login path

---
+++ e2e/auth.e2e.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+
+const createApp = require('../src/app');
+// Import the models from the DB
+const { models } = require('./../src/db/sequelize');
+
+let app = null;
+let server = null;
+let api;
+
+beforeAll(() => {
+  app = createApp();
+
+  server = app.listen(9000);
+
+  api = request(app);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+describe('test for /auth path', () => {
+  describe('POST /login', () => {
+    it('should return 401', async () => {
+      const inputUser = {
+        email: 'email@fake.com',
+        password: 'admin123',
+      };
+
+      const { statusCode } = await api
+        .post('/api/v1/auth/login')
+        .send(inputUser);
+
+      expect(statusCode).toBe(401);
+    });
+
+    it('should return 200', async () => {
+      const user = await models.User.findByPk('1');
+      const inputUser = {
+        email: user.email,
+        password: 'admin123',
+      };
+
+      const { statusCode, body } = await api
+        .post('/api/v1/auth/login')
+        .send(inputUser);
+
+      expect(statusCode).toBe(200);
+      expect(body.access_token).toBeTruthy();
+      expect(body.user.email).toEqual(user.email);
+      expect(body.user.password).toBeUndefined();
+    });
+  });
+});
